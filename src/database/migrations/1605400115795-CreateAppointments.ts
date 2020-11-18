30613b1e61e9a3e1ca6bf75b9508713bbe280706
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2020-11-17 21:53:01 -0300

    chore: Inserçao da conexao com DB

--- src/database/migrations/1605400115795-CreateAppointments.ts
+++ src/database/migrations/1605400115795-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1605400115795
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -10,12 +10,6 @@ class Appointment {
 
   @Column('timestamp with time zone')
   date: Date;
-
-  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
-  //   this.id = uuid();
-  //   this.provider = provider;
-  //   this.date = date;
-  // }
 }
 
 export default Appointment;
