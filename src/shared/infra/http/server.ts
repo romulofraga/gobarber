f1bfcb8d280897716840d65f7c497bb2f1cbd1c2
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2021-01-21 23:19:55 -0300

    Chore: Refatoraçao dos diretorios - arquitetura

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,19 +1,20 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors'
 import 'express-async-errors';
-import routes from './routes/index';
-import './database';
 
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import routes from './routes/index';
+import uploadConfig from 'config/upload';
+import AppError from '@shared/errors/AppError';
+
+import '@shared/infra/typeorm/index';
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
-
 app.use('/files', express.static(uploadConfig.directory));
-
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
