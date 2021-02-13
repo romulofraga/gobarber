86037655677ed6d16bc536a4a7be532000db32f7
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2021-02-13 17:35:46 -0300

    migrations userTokens e registro de depencencias

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,8 +10,8 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
-// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-// import UserTokensRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentRepository>(
   'AppointmentsRepository',
@@ -21,4 +21,9 @@ container.registerSingleton<IAppointmentRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UsersTokenRepository',
+  UserTokensRepository,
 );
